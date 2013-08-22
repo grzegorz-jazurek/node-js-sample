d30adcfe897b9b8f17a3fb874bf4aa6bf3190828
OpenShift guest <grzegorz.jazurek@gmail.com>
2013-08-22 08:01:04 -0400

    return index.html file stream

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require("fs");
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Grzes!');
+    var fileBody = fs.readFileSync('index.html');
+    
+    response.send(fileBody.toString());
 });
 
 var port = process.env.PORT || 5000;
